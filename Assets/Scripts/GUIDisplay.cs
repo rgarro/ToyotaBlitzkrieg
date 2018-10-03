4dd2de3e695284b52fa8389f681d3f8b2fb59035
rgarro <rgarro@gmail.com>
2018-10-03 16:34:45 -0600

    have run you down into the ground spread disease about you over town ...

---
+++ Assets/Scripts/GUIDisplay.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUIDisplay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnGUI ()
+	{
+		// Make a background box
+		GUI.Box(new Rect(10,10,100,90), "Loader Menu");
+
+		// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
+		if(GUI.Button(new Rect(20,40,80,20), "Blue Tundra"))
+		{
+			//Application.LoadLevel(1);
+		}
+
+		// Make the second button.
+		if(GUI.Button(new Rect(20,70,80,20), "Red Hilux")) 
+		{
+			//Application.LoadLevel(2);
+		}
+	}
+}
