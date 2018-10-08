a7f6d7f8b2484c88734cdfd0d7010aab4cd77a0e
rgarro <rgarro@gmail.com>
2018-10-08 17:00:40 -0600

    queen astarte red lipstick ..

--- Assets/Scripts/GUIDisplay.cs
+++ Assets/Scripts/GUIDisplay.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class GUIDisplay : MonoBehaviour {
 
+
+	public Texture crispyLogo;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -16,17 +19,19 @@ public class GUIDisplay : MonoBehaviour {
 
 	void OnGUI ()
 	{
+		GUI.Box (new Rect (10,10,100,200), new GUIContent(crispyLogo));
 		// Make a background box
-		GUI.Box(new Rect(10,10,100,90), "Loader Menu");
+		GUI.Box(new Rect(10,115,100,90), "Loader Menu");
+		//GUI.Button (new Rect (10,10,100,20), new GUIContent ("Click me", icon, "This is the tooltip"));
 
 		// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
-		if(GUI.Button(new Rect(20,40,80,20), "Blue Tundra"))
+		if(GUI.Button(new Rect(20,145,80,20), "Blue Tundra"))
 		{
 			//Application.LoadLevel(1);
 		}
 
 		// Make the second button.
-		if(GUI.Button(new Rect(20,70,80,20), "Red Hilux")) 
+		if(GUI.Button(new Rect(20,175,80,20), "Red Hilux")) 
 		{
 			//Application.LoadLevel(2);
 		}
