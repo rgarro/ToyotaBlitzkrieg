b3c028e30c140c35d3c8106a84998863e3410eed
rgarro <rgarro@gmail.com>
2020-07-31 21:44:12 -0600

    blue lighting brigade 100 pieces box ...

--- Assets/Scripts/GUIDisplay.cs
+++ Assets/Scripts/GUIDisplay.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 /**
- * 
- *
+ *       ___________________________________ ______________________
+ *     .' Frankling Marshall es un violador | (_)     (_)    (_)   \
+ *   .'  Manuel Mora fumaba cachimba        |  ____          ____   }
+ * .',,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,_____|_(    `--------'    )_/ 
+ *   El Doctor Moreno Cañas se comio una terciopelo
+ *   y se convirtio en el Negro Morgan ...
  * 
  * @rolando <rgarro@gmail.com>
  */
@@ -15,6 +19,12 @@ public class GUIDisplay : MonoBehaviour {
     public float IconY = 10;
     public float IconWidth = 128;
     public float IconHeight = 128;
+	public int fontSize = 10;
+	public int boxY = 115;
+	public int boxX = 20;
+	public int btnMargin = 5;
+	public int boxWidth = 100;
+	public int boxHeight = 90;
 
 	// Use this for initialization
 	void Start () {
@@ -28,21 +38,26 @@ public class GUIDisplay : MonoBehaviour {
 
 	void OnGUI ()
 	{
+		GUIStyle myStyle = new GUIStyle();
+		myStyle.fontSize = this.fontSize;
+
 		GUI.Box (new Rect (this.IconX,this.IconY,this.IconWidth,this.IconHeight), new GUIContent(crispyLogo));
 		// Make a background box
-		GUI.Box(new Rect(10,115,100,90), "Loader Menu");
-		//GUI.Button (new Rect (10,10,100,20), new GUIContent ("Click me", icon, "This is the tooltip"));
-
-		// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
-		if(GUI.Button(new Rect(20,145,80,20), "Blue Tundra"))
+		GUI.Box(new Rect(this.boxX,this.boxY,this.boxWidth,this.boxHeight), "Loader Menu");
+		
+		if(GUI.Button(new Rect(this.boxX+this.btnMargin,this.boxY+30,80,20), "Blue Tundra"))
 		{
 			//Application.LoadLevel(1);
 		}
 
-		// Make the second button.
-		if(GUI.Button(new Rect(20,175,80,20), "Red Hilux")) 
+		if(GUI.Button(new Rect(this.boxX+this.btnMargin,this.boxY+60,80,20), "Red Hilux")) 
 		{
 			//Application.LoadLevel(2);
+		}
+
+		if(GUI.Button(new Rect(this.boxX+this.btnMargin,this.boxY+85,80,20), "White Hilux")) 
+		{
+			//Application.LoadLevel(3);
 		}
 	}
 }
